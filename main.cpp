577e27beee866c7bf9a27668286e3148e5d5abde
WrenHainsworth <hainsworthwren@gmail.com>
2022-04-01 22:11:05 +1100

    AND gate

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "MLP.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "\n\n--------Logic Gate Example----------------\n\n";
+    Perceptron *p = new Perceptron(2);
+
+    p->set_weights({10,10,-15}); //AND
+
+    cout << "Gate: "<<endl;
+    cout<<p->run({0,0})<<endl;
+    cout<<p->run({0,1})<<endl;
+    cout<<p->run({1,0})<<endl;
+    cout<<p->run({1,1})<<endl;
+
 }
